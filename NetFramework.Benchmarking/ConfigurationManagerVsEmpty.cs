9e28c698efa2d3af8d82ecaf20b12fe0a077dac2
e.mishustin <e.mishustin@tinkoff.ru>
2023-02-09 18:49:05 +0300

    master
    Add NetFramework.Benchmarking
    Add ConfigurationManager.AppSettings

---
+++ NetFramework.Benchmarking/ConfigurationManagerVsEmpty.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace NetFramework.Benchmarking
+{
+	[MemoryDiagnoser]
+	[DisassemblyDiagnoser]
+	[SimpleJob(RuntimeMoniker.HostProcess)]
+	public class ConfigurationManagerVsEmpty
+	{
+		[Params(10000)]
+		public int N { get; set; }
+
+		[Benchmark]
+		public void ConfigurationManager()
+		{
+			for (var i = 0; i < N; i++)
+			{
+				var config = System.Configuration.ConfigurationManager.AppSettings["someConfig"];
+			}
+		}
+
+		[Benchmark(Baseline = true)]
+		public void Empty()
+		{
+			for (var i = 0; i < N; i++)
+			{
+				
+			}
+		}
+	}
+}
